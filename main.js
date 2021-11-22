823c07f614c0f6f0a5ca8ca749547c059ffe6d36
AnasAbdelmajeed <anas.a.alomari@gmail.com>
2021-11-22 22:21:53 +0200

    Pulse check

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log(arrowTitle)
+/* START CODE UNDER THIS LINE */
+
+////////////////////////////////////////////////////
+/////////////////  Pulse Check   ///////////////////
+//1 Write a function that accepts two arguments, number and callback, and using the callback return the number squared.
+
+const exampleFunction = function (x, y, callback) {
+    return callback (Math.pow(x , y))
+};
+
+
+////////////////////////////////////////////////////
+
+//2- 
+
